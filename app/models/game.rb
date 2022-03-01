dc1752945f1d2c1ea9c73ac9b6de2321382147fb
alexandra <alexandraschurman@gmail.com>
2022-03-01 11:50:05 -0800

    commit tree 0a328c185238a20a7758d9cae204cd406149bc72

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,7 @@
 class Game < ActiveRecord::Base
   has_many :reviews
+  has_many :users, through: :reviews
+  # above, the order matters. the 1st has many references the table above the secodn has many 
+  # note, these are still methods
+  
 end
